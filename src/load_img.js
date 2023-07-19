274c99f3962417cbda8230ba087a740d99100f00
andriano <andrianow817@gmail.com>
2023-07-19 09:44:06 +0700

    add line on banner and count content

--- src/load_img.js
+++ src/load_img.js
@@ -1,7 +1,8 @@
 import Logo from "../asset/Logo .svg"
-
+import BannerPicture from "../asset/BannerPicture.svg"
 function load_img(classname, img_name, order = 0) {
     return document.getElementsByClassName(classname)[order].src = img_name
 }
 
 load_img("icon_logo", Logo)
+load_img("banner_img", BannerPicture)
