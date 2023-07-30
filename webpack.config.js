d98586d33b09a7b837065018055e01aa82e0302c
andriano <andrianow817@gmail.com>
2023-07-30 09:06:52 +0700

    add icon and delete load img js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
 module.exports = {
   entry: {
     main: "./src/index.js",
-    img_loader: {
-      import: "./src/loaders/load_img.js",
-      dependOn: "img",
-    },
-    img: "./src/loaders/img-loader.js",
   },
   output: {
     clean: true,
@@ -18,15 +12,11 @@ module.exports = {
     rules: [
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
       { test: /\.(png|svg|jpg|jpeg)$/i, type: "asset/resource" },
+      { test: /\.html$/i, use: "html-loader" },
     ],
   },
   optimization: {
     minimizer: [new CssMinimizerPlugin()],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
-  resolve: {
-    alias: {
-      asset: path.resolve(__dirname, "./src/asset"),
-    },
-  },
 };
